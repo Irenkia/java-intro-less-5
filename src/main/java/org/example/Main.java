9f6df4a03f5d0b00043618fbf54c1c26a6753380
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-01-16 21:28:46 +0100

    created classe Triangle and created method toString() of this class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        forTriangle();
+    }
+    public static void forTriangle(){
+        Triangle triangle = new Triangle(3.0, 5.0);
+        System.out.println(triangle.toString());;
     }
 }
