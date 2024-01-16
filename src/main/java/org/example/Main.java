80240c151e6b8db48706a32d54e704dfc9495af7
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-01-16 12:18:40 +0100

    created classe Animal and classes Dog, Cat inherit this class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        forAnimal();
+    }
+    public static void forAnimal(){
+        Animal dog = new Dog("Toby");
+        Animal cat = new Cat("Lola");
+        System.out.println("Our dog's name is " + dog.getName());
+        System.out.println("Our cat's name is " + cat.getName());
     }
 }
