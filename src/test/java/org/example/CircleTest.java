1a39f0332436d6f83ce05e07cca531afec02be81
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-01-16 11:16:54 +0100

    created interface Shape and classes Circle, Triangle implementing this interface and tests for these classes

---
+++ src/test/java/org/example/CircleTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CircleTest {
+    @Test
+    public void shouldAreaCircle() {
+        //given
+        Shape circle = new Circle(4.0);
+
+        //when
+        double result = circle.getArea();
+
+        //then
+        Assertions.assertEquals(50.27, result);
+    }
+    @Test
+    public void shouldPerimeterCircle() {
+        //given
+        Shape circle = new Circle(4.0);
+
+        //when
+        double result = circle.getPerimeter();
+
+        //then
+        Assertions.assertEquals(25.13, result);
+    }
+}
