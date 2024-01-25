06b51ac627fe91789330514771f1d2a55d73a8c4
Iryna Tiutiun <tiutiuniryna@gmail.com>
2024-01-25 18:19:56 +0100

    formatted the files

--- src/main/java/org/example/BillCalculator.java
+++ src/main/java/org/example/BillCalculator.java
@@ -1,23 +1,27 @@
 package org.example;
 
 public class BillCalculator {
-    public static double calculateBill(double bill, float payOfServices){
+    public static double calculateBill(double bill, float payOfServices) {
         return roundResult(bill + payServices(bill, payOfServices));
     }
 
-    public static double calculateBill(double bill, float payOfServices, double discount){
+    public static double calculateBill(double bill, float payOfServices, double discount) {
         return roundResult((bill - calculateDiscount(bill, discount)) + payServices(bill, payOfServices));
     }
-    public static double calculateBill(double bill, float payOfServices, short packagingFee){
-        return roundResult(bill + payServices(bill, payOfServices) + (double)packagingFee);
+
+    public static double calculateBill(double bill, float payOfServices, short packagingFee) {
+        return roundResult(bill + payServices(bill, payOfServices) + (double) packagingFee);
     }
 
-    private static double payServices(double bill, float payOfServices){
+    private static double payServices(double bill, float payOfServices) {
         return (payOfServices / 100) * bill;
     }
 
-    private static double calculateDiscount(double bill, double discount){return (discount / 100) * bill;}
-    private static double roundResult(double result){
+    private static double calculateDiscount(double bill, double discount) {
+        return (discount / 100) * bill;
+    }
+
+    private static double roundResult(double result) {
         return Math.round(result * 100.0) / 100.0;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,13 +9,16 @@ public class Main {
         float payOfServices = 0.3f;
         double discount = 1.5;
         short packagingFee = 5;
+
         System.out.println("\nOur bill = " + bill);
         System.out.print("\nWith service fee (0.3%) : 200 + 0.60 = ");
         System.out.print(BillCalculator.calculateBill(bill, payOfServices));
+
         System.out.println("\n\nWith service fee (0.3%) and minus discount (1.5%) ");
         System.out.println("the discount applies only to the invoice amount and not to the service fee :");
         System.out.print("(200 - 3) + 0.60 = ");
         System.out.println(BillCalculator.calculateBill(bill, payOfServices, discount));
+
         System.out.print("\n\nWith service fee (0.3%) and takeaway packaging fee (fix price = 5) : 200 + 0.60 + 5 = ");
         System.out.println(BillCalculator.calculateBill(bill, payOfServices, packagingFee));
     }
